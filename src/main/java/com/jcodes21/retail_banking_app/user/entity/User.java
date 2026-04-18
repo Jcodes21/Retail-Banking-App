ef0a6175e60d29349707443ba4bcbd11aec75d14
Joseph halden <josephhalden1@gmail.com>
2026-04-18 01:33:06 +0100

    added jpa

---
+++ src/main/java/com/jcodes21/retail_banking_app/user/entity/User.java
@@ -0,0 +1,27 @@
+package com.jcodes21.retail_banking_app.user.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String firstName;
+    private String lastName;
+    private String middleNames;
+    private LocalDate dateOfBirth;
+    private String address;
+    private String postCode;
+    private String email;
+    private String userName;
+    private String password;
+    private LocalDateTime createdAt;
+    private Boolean isActive;
+    private Boolean isFrozen;
+}
