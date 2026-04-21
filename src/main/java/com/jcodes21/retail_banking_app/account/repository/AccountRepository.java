7e5f6661403d7342a65912b57acd1b6e77778969
Joseph halden <josephhalden1@gmail.com>
2026-04-21 18:39:38 +0100

    fixing file

--- src/main/java/com/jcodes21/retail_banking_app/account/repository/AccountRepository.java
+++ src/main/java/com/jcodes21/retail_banking_app/account/repository/AccountRepository.java
@@ -2,10 +2,9 @@ package com.jcodes21.retail_banking_app.account.repository;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.jcodes21.retail_banking_app.account.entity.Account;
+import java.util.UUID;
 
 // JPA/Spring Data reads these custom methods and generates the implementation.
-public interface AccountRepository extends JpaRepository<Account, Long> {
-    Optional<Account> FindByAccount(String account);
-
-    boolean ExistByEmail(String email);
+public interface AccountRepository extends JpaRepository<Account, UUID> {
+    Optional<Account> findByAccountNumber(String account);
 }
