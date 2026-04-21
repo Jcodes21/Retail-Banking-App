d4e1e63306a5870d0ef29b43baf51f1a31e03ba6
Joseph halden <josephhalden1@gmail.com>
2026-04-21 21:19:22 +0100

    creating account service file

--- src/main/java/com/jcodes21/retail_banking_app/account/service/AccountService.java
+++ src/main/java/com/jcodes21/retail_banking_app/account/service/AccountService.java
@@ -1,8 +1,49 @@
 package com.jcodes21.retail_banking_app.account.service;
+import com.jcodes21.retail_banking_app.account.entity.AccountType;
+import java.math.BigDecimal;
 
 public class AccountService {
+    AccountType accountType;
+    String accountNumber;
+    String sortCode;
+    BigDecimal balance;
+    String firstName;
+    String LastName;
+    String middleNames;
+    String email;
+
     // methods
     public static void createAccount() {
 
+
+        take the user details and add it to the account record being created in the db
+        generate account number
+        generate sort code
+        db.save();
     };
+
+    public static void getAccount(){
+
+    }
+
+    public static void getAllAccounts{
+
+    }
+
+    public void freezeAccount(){
+
+    }
+
+    public static void deleteAccount(){
+
+    }
+
+    public static void deposit(){
+
+    }
+
+    public static void withdraw(){
+    }
+
+
 }
